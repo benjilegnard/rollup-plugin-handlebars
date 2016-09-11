3bbb6941e4e6daef8be8c919c4c26ba6bf679a9b
Drew Drew, Drew <Lemmmy@users.noreply.github.com>
2016-09-11 12:31:50 +0100

    Fixed 'require is not defined'

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ export default function (options = {}) {
       //console.dir(templateFunction.toString());
       let compiled = '';
       //see https://github.com/epeli/node-hbsfy/blob/master/runtime.js for inspiration
-      compiled += "const HandlebarsCompiler = require('handlebars/runtime')['default'];\n";
+      compiled += "import HandlebarsCompiler from 'handlebars/runtime';\n";
       compiled += "export default HandlebarsCompiler.template(" + templateFunction.toString() + ");\n";
       //console.log(compiled);
       return {
